904b72a8e700958dd87970a12c561706e7df0dd5
Stephen Schuez <stephen.schuez@gmail.com>
2021-03-03 09:20:54 +0100

    added question types logic to model and to form, added some styling to forms

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,4 +8,29 @@ class Question < ApplicationRecord
 	def previous_question
 	  Question.where(order: self.order - 1).first
 	end
+
+	def question_type?
+		case self.question_type
+		when "text" 
+			:text
+		when "integer"
+			:integer
+		when "boolean"
+			:boolean
+		when "radio_buttons"
+			:radio_buttons
+		when "check_boxes"
+			:check_boxes
+		when "select"
+			:select
+		when "file"
+			:file
+		when "date"
+			:date
+		when "password"
+			:password
+		else
+			:string 
+		end
+	end
 end
