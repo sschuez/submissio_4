2cb0bd08957df27d3fa5456e305c2dc06d5497a2
Stephen Schuez <stephen.schuez@gmail.com>
2021-03-08 16:18:01 +0100

    added form.io page and link as experiment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def form_io
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
 	resources :questions, only: [ :index, :create, :update, :destroy ]
 
+	get 'form_io', to: 'pages#form_io'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
